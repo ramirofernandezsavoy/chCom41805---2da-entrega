2ae58f9f9457c611a7d0471723eced82fe96d36c
Ramiro Savoy <rawii.k@gmail.com>
2022-10-23 12:01:25 -0300

    suspectjs to local storage

--- js/app.js
+++ js/app.js
@@ -1,165 +1,4 @@
-// Clase constructora de las personas.
-class Persona {
-  constructor(id, nombre, ojos, sexo, cabello, apodo, culpable) {
-    this.id = id;
-    this.nombre = nombre;
-    this.ojos = ojos;
-    this.sexo = sexo;
-    this.cabello = cabello;
-    this.apodo = apodo;
-    this.culpable = false;
-  }
-}
 
-// Creacion de distintos personajes.
-const personaje1 = new Persona(
-    1,
-    "Fernando Tasca",
-    "marron",
-    "masculino",
-    "castaño",
-    "cabezon"
-  ),
-  personaje2 = new Persona(
-    2,
-    "Nicolas Aubin",
-    "celeste",
-    "masculino",
-    "rubio",
-    "el viejo"
-  ),
-  personaje3 = new Persona(
-    3,
-    "Damian Massolo",
-    "negro",
-    "masculino",
-    "negro",
-    "lince"
-  ),
-  personaje4 = new Persona(
-    4,
-    "Facundo Bajano",
-    "marron",
-    "masculino",
-    "colorado",
-    "batracio"
-  ),
-  personaje5 = new Persona(
-    5,
-    "Gabriel Tolmasky",
-    "verde",
-    "masculino",
-    "negro",
-    "judio"
-  ),
-  personaje6 = new Persona(
-    6,
-    "Lucas Romero",
-    "verde",
-    "masculino",
-    "castaño",
-    "no tiene"
-  ),
-  personaje7 = new Persona(
-    7,
-    "Valeria Singh",
-    "marron",
-    "femenino",
-    "rubio",
-    "aida"
-  ),
-  personaje8 = new Persona(
-    8,
-    "Pamela Aldaz",
-    "verde",
-    "femenino",
-    "castaño",
-    "pam"
-  ),
-  personaje9 = new Persona(
-    9,
-    "Evelyn Gout",
-    "negro",
-    "femenino",
-    "negro",
-    "eve"
-  ),
-  personaje10 = new Persona(
-    10,
-    "Glenda Romano",
-    "negro",
-    "femenino",
-    "rosa",
-    "glen"
-  );
-
-// Creacion de array de sospechosos.
-const sospechosos = [
-  personaje1,
-  personaje2,
-  personaje3,
-  personaje4,
-  personaje5,
-  personaje6,
-  personaje7,
-  personaje8,
-  personaje9,
-  personaje10,
-];
-
-// Seleccion del asesino.
-function randomN() {
-  return Math.floor(Math.random() * sospechosos.length + 1);
-}
-const asesinoNum = randomN();
-console.log(asesinoNum);
-
-let asesino = "";
-switch (asesinoNum) {
-  case 1:
-    personaje1.culpable = true;
-    asesino = sospechosos[0];
-    break;
-  case 2:
-    personaje2.culpable = true;
-    asesino = sospechosos[1];
-    break;
-  case 3:
-    personaje3.culpable = true;
-    asesino = sospechosos[2];
-    break;
-  case 4:
-    personaje4.culpable = true;
-    asesino = sospechosos[3];
-    break;
-  case 5:
-    personaje5.culpable = true;
-    asesino = sospechosos[4];
-    break;
-  case 6:
-    personaje6.culpable = true;
-    asesino = sospechosos[5];
-    break;
-  case 7:
-    personaje7.culpable = true;
-    asesino = sospechosos[6];
-    break;
-  case 8:
-    personaje8.culpable = true;
-    asesino = sospechosos[7];
-    break;
-  case 9:
-    personaje9.culpable = true;
-    asesino = sospechosos[8];
-    break;
-  case 10:
-    personaje10.culpable = true;
-    asesino = sospechosos[9];
-    break;
-  default:
-    alert("Ocurrio un error. Intente de nuevo");
-    break;
-}
 
 // Identificacion y funcionalidad de Objetos
 const witnessButton = document.querySelector("#witnessButton"),
@@ -167,20 +6,19 @@ const witnessButton = document.querySelector("#witnessButton"),
   camButton = document.querySelector("#camButton"),
   interlocutor = document.querySelector("#text1"),
   mensaje = document.querySelector("#text2");
-  
-  
 
+// Funcionalidad de los botones.
 witnessButton.addEventListener("click", () => {  
   interlocutor.innerText = `La señora Pedraza dice:`
-  mensaje.innerText = `Esa noche yo escuche la voz de un personaje de sexo ${sexo}, que le hablaba a alguien por telefono. Mire por la ventana y me parecio ver un buzo con capucha color ${ropa}. Finalmente, pasado un rato, me acerque a la casa de Hector para ver si estaba bien y encontre el cadaver, en la mano tenia un mechon de pelo ${cabello}. Le juro que no se nada mas.`
+  mensaje.innerText = `Esa noche yo escuche la voz de un personaje de sexo ${asesino.sexo}, que le hablaba a alguien por telefono. Subí el volumen de la tele, pues me molestaba, aunque eso no me impidio escuchar un grito. Me asusté. Me quede mirando a escondidas detras de la cortina y vi salir una figura de la casa de Hector. Finalmente, pasado un rato, me arme de valor y me acerque a la casa para ver si el estaba bien, alli encontre el cadaver, en la mano tenia un mechon de pelo ${asesino.cabello}. Le juro que no se nada mas.`
 });
 msjButton.addEventListener("click", () => {
-  interlocutor.innerText = `Nico`
-  mensaje.innerText = `La concha de tu madre`
+  interlocutor.innerText = `Juan, tu compañero del cuartel te escribe:`
+  mensaje.innerText = `Hey, realizamos algunas investigaciones, corre el rumor en la calle de que el asesino escapo hacia los barrios bajos y se esconde alli. Aparentemente se hace apodar ${asesino.apodo}`
 });
 camButton.addEventListener("click", () => {
-  interlocutor.innerText = `Nico`
-  mensaje.innerText = `La concha de tu madre`
+  interlocutor.innerText = `La camara de seguridad del barrio cerrado captó:`
+  mensaje.innerText = `Durante la noche del asesinato, el barrio estaba muy tranquilo, pero si analizas detenidamente, ves moverse una sombra entre los arbustos. Tiempo despues, se ve abrir la puerta trasera de la casa y una figura oscura emerge de la misma. No se llega a apreciar muy bien su ropa, pero se acerca hacia la valla, levanta la cabeza para mirar en la casa del vecino y al hacerlo un rayo de luz pega de lleno en la capucha, de color ${asesino.ropa} y unos penetrantes ojos color ${asesino.ojos}. Se escucha un ruido cerca y la figura se pierde en las sombras de la noche.`
 });
 
 
@@ -324,10 +162,3 @@ function acusarAsesino() {
     );
   }
 }
-
-// LOGS USADOS PARA IR VERIFICANDO LAS DISTINTAS ETAPAS DEL PROCESO.
-
-console.log(sospechosos);
-console.log(expedienteFiltrado);
-console.log(asesino);
-console.log(asesinoNum);
---
+++ js/suspects.js
@@ -0,0 +1,187 @@
+// Clase constructora de las personas.
+class Persona {
+  constructor(id, nombre, ojos, sexo, cabello, apodo, ropa, culpable) {
+    this.id = id;
+    this.nombre = nombre;
+    this.ojos = ojos;
+    this.sexo = sexo;
+    this.cabello = cabello;
+    this.apodo = apodo;
+    this.ropa = ropa;
+    this.culpable = false;
+  }
+}
+
+// Creacion de distintos personajes.
+const personaje1 = new Persona(
+    1,
+    "Fernando Tasca",
+    "marron",
+    "masculino",
+    "castaño",
+    "cabezon",
+    "negro"
+  ),
+  personaje2 = new Persona(
+    2,
+    "Nicolas Aubin",
+    "celeste",
+    "masculino",
+    "rubio",
+    "el viejo",
+    "marron con rayas"
+  ),
+  personaje3 = new Persona(
+    3,
+    "Damian Massolo",
+    "negro",
+    "masculino",
+    "negro",
+    "lince",
+    "negro"
+  ),
+  personaje4 = new Persona(
+    4,
+    "Facundo Bajano",
+    "marron",
+    "masculino",
+    "colorado",
+    "batracio",
+    "gris"
+  ),
+  personaje5 = new Persona(
+    5,
+    "Gabriel Tolmasky",
+    "verde",
+    "masculino",
+    "negro",
+    "judio",
+    "verde"
+  ),
+  personaje6 = new Persona(
+    6,
+    "Lucas Romero",
+    "verde",
+    "masculino",
+    "castaño",
+    "Ele",
+    "rojo"
+  ),
+  personaje7 = new Persona(
+    7,
+    "Valeria Singh",
+    "marron",
+    "femenino",
+    "rubio",
+    "aida",
+    "azul"
+  ),
+  personaje8 = new Persona(
+    8,
+    "Pamela Aldaz",
+    "verde",
+    "femenino",
+    "castaño",
+    "pam",
+    "gris"
+  ),
+  personaje9 = new Persona(
+    9,
+    "Evelyn Gout",
+    "negro",
+    "femenino",
+    "negro",
+    "eve",
+    "azul"
+  ),
+  personaje10 = new Persona(
+    10,
+    "Glenda Romano",
+    "negro",
+    "femenino",
+    "rosa",
+    "glen",
+    "negro"
+  );
+
+// Creacion de array de sospechosos.
+const sospechosos = [
+  personaje1,
+  personaje2,
+  personaje3,
+  personaje4,
+  personaje5,
+  personaje6,
+  personaje7,
+  personaje8,
+  personaje9,
+  personaje10,
+];
+
+// Seleccion del asesino.
+function randomN() {
+  return Math.floor(Math.random() * sospechosos.length + 1);
+}
+
+let asesino = "";
+const asesinoNum = randomN();
+
+console.log(asesinoNum);
+
+function pickAssa() {
+    
+switch (asesinoNum) {
+  case 1:
+    personaje1.culpable = true;
+    asesino = sospechosos[0];    
+    break;
+  case 2:
+    personaje2.culpable = true;
+    asesino = sospechosos[1];    
+    break;
+  case 3:
+    personaje3.culpable = true;
+    asesino = sospechosos[2];
+    break;
+  case 4:
+    personaje4.culpable = true;
+    asesino = sospechosos[3];
+    break;
+  case 5:
+    personaje5.culpable = true;
+    asesino = sospechosos[4];
+    break;
+  case 6:
+    personaje6.culpable = true;
+    asesino = sospechosos[5];
+    break;
+  case 7:
+    personaje7.culpable = true;
+    asesino = sospechosos[6];
+    break;
+  case 8:
+    personaje8.culpable = true;
+    asesino = sospechosos[7];
+    break;
+  case 9:
+    personaje9.culpable = true;
+    asesino = sospechosos[8];
+    break;
+  case 10:
+    personaje10.culpable = true;
+    asesino = sospechosos[9];
+    break;
+  default:
+    alert("Ocurrio un error. Intente de nuevo");
+    break;
+}
+}
+
+if (localStorage.getItem("culpable")) {
+  asesino = JSON.parse(localStorage.getItem("culpable"));
+  } else {
+  pickAssa ();
+  localStorage.setItem("culpable", JSON.stringify(asesino));
+}
+
+console.log(asesino);
